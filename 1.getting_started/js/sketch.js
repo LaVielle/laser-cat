fffe57120a4d2b6a7414eacb5f8a4dd96199b209
Samuel Mann <samuelcmann@gmail.com>
2017-10-16 05:40:13 -0400

    adding part 1

---
+++ 1.getting_started/js/sketch.js
@@ -0,0 +1,14 @@
+// The setup() function
+function setup(){
+
+   // create HTML canvas element
+   var canvas = createCanvas(640, 360);
+
+   // Making background black
+   background(0);
+}
+
+// The draw() function loops at 30fps. This is where interaction and animation happens.
+function draw() {
+   // Nothing here yet
+}
