e62f0b4b5ab4ddd6a01c6fc3b2ce7e15ba36814a
Samuel Mann <samuelcmann@gmail.com>
2017-10-17 06:26:43 -0400

    adding chap 4

---
+++ 4.eyes/js/sketch.js
@@ -0,0 +1,28 @@
+var catImg;
+
+// Declaring arrays that will contain eye and laser objects
+var eyes = [];
+var lasers = [];
+
+function setup(){
+	var canvas = createCanvas(640, 360);
+	background(0);
+
+   catImg = loadImage('assets/cat.jpg');
+}
+
+function draw(){
+	background(0);
+
+   image(catImg, 0, 0);
+
+	for (var i = 0; i < eyes.length; i++){
+	   eyes[i].show(); // call the show() method for each i-th element in the eyes array
+	}
+
+}
+
+function mousePressed(){
+   eyes.push(new eye(mouseX, mouseY)); // Create a new element in the eyes array: new eye with mouseX/Y as x/y position
+   console.log("created eye"); // Log that an eye was created.
+}
